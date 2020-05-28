a9625b2de647377908fb1118bd405f38f06d3743
ApisLineolata <ApisLineolata@gmail.com>
2020-05-27 22:26:05 -0400

    Create failing test for sentence splitter
    - Will be the logic that decides what is and is not a sentence
    
    - Can be made to recognise more complex sentence structure as time progresses

---
+++ AlphabetSentences/SentenceSplitter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphabetSentences
+{
+    public class SentenceSplitter
+    {
+        public List<string> Split(string sentenceToSplit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlphabetSentencesTests/SentenceSplitterTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using AlphabetSentences;
+using NUnit.Framework;
+
+namespace AlphabetSentencesTests
+{
+    [TestFixture]
+    public class SentenceSplitterTests
+    {
+        [Test]
+        public void SplitterSplitsPeriodsTest()
+        {
+            SentenceSplitter splitter = new SentenceSplitter();
+            var testSentence = "This will be. Two sentences.";
+            List<string> expectedSentences = new List<string> {"This will be.", "Two sentences."};
+            List<string> sentences = splitter.Split(testSentence);
+            Assert.That(sentences, Is.EquivalentTo(expectedSentences));
+        }
+    }
+}
