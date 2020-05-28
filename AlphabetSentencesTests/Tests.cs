a18ac7126394c9ff7b9d73d314bac0605793ba68
ApisLineolata <ApisLineolata@gmail.com>
2020-05-27 22:15:39 -0400

    Create test project

---
+++ AlphabetSentencesTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AlphabetSentencesTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
