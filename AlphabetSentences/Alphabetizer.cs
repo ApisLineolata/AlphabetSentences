cb1fdd96d139a15e79aec70f325ccc8bd05ee769
ApisLineolata <ApisLineolata@gmail.com>
2020-05-27 22:37:31 -0400

    Add base test for Alphabetizer
    - Doensn't need to be super complex yet
    
    - Will look at tests for longer strings in future

---
+++ AlphabetSentences/Alphabetizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphabetSentences
+{
+    public class Alphabetizer
+    {
+        public List<string> Alphabetize(List<string> stringsToAlphabetize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlphabetSentencesTests/AlphabetizerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using AlphabetSentences;
+using NUnit.Framework;
+
+namespace AlphabetSentencesTests
+{
+    [TestFixture]
+    public class AlphabetizerTests
+    {
+        [Test]
+        public void AlphabatizesGivenStrings()
+        {
+            List<string> testSentences = new List<string> {"f", "d", "s", "a"};
+            List<string> expectedSentences = new List<string> {"a", "d", "f", "s"};
+
+            Alphabetizer alphabetizer = new Alphabetizer();
+            List<string> alphabetizedSentances = alphabetizer.Alphabetize(testSentences);
+
+            Assert.That(alphabetizedSentances, Is.EqualTo(expectedSentences));
+        }
+    }
+}
