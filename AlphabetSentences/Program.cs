8ad25e019fe0621eed99b7cd112409aa464c2905
ApisLineolata <ApisLineolata@gmail.com>
2020-05-27 22:49:41 -0400

    Create initial program run

--- AlphabetSentences/Program.cs
+++ AlphabetSentences/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace AlphabetSentences
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AlphabetSentences
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var storyLines = File.ReadAllLines("../../ShortStory.txt");
+
+            SentenceSplitter splitter = new SentenceSplitter();
+            List<string> splitSentances = new List<string>();
+            foreach (var storyLine in storyLines) splitSentances.AddRange(splitter.Split(storyLine));
+
+            Alphabetizer alphabetizer = new Alphabetizer();
+            var alphabatizedSentences = alphabetizer.Alphabetize(splitSentances);
+
+            Console.ReadLine();
         }
     }
 }
