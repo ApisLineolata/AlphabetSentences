410ce83a92f495be3e04ba85156812ccfa9536d2
ApisLineolata <ApisLineolata@gmail.com>
2020-05-27 22:52:22 -0400

    Add test to check for quotation mark removal

--- AlphabetSentencesTests/SentenceSplitterTests.cs
+++ AlphabetSentencesTests/SentenceSplitterTests.cs
@@ -8,6 +8,17 @@ namespace AlphabetSentencesTests
     public class SentenceSplitterTests
     {
         [Test]
+        public void QuotationMarksAreRemovedTest()
+        {
+            SentenceSplitter splitter = new SentenceSplitter();
+            var testSentence = "This will be. \"Four sentences. Also a quote.\" Trailing too.";
+            List<string> expectedSentences = new List<string>
+                {"This will be.", "Four sentences.", "Also a quote.", "Trailing too."};
+            List<string> sentences = splitter.Split(testSentence);
+            Assert.That(sentences, Is.EquivalentTo(expectedSentences));
+        }
+
+        [Test]
         public void SplitterSplitsPeriodsTest()
         {
             SentenceSplitter splitter = new SentenceSplitter();
